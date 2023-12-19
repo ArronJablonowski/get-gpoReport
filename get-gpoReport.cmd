7b7d85c6978dbacd814271787384efee04fb91df
Arron Jablonowski <77128909+ArronJablonowski@users.noreply.github.com>
2023-12-18 18:28:34 -0600

    Update get-gpoReport.cmd

--- get-gpoReport.cmd
+++ get-gpoReport.cmd
@@ -1,3 +1,3 @@
-REM Create an HTML report of all GPOs in the current directory. 
+REM Create an HTML report of all GPOs. Report outputs to the current directory. 
 
 START Powershell.exe -WindowStyle hidden -executionpolicy bypass -Command "Get-GPOReport -All -ReportType Html -Path '%~dp0\All-GPOs.html' "
